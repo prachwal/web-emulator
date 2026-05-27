d853b0e045b2196cc988b2bbbe1093bfb63bc90d
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2026-05-27 10:49:34 +0200

    fix(kaypro): Kaypro II PAR 0.5 for 2x scale, Kaypro 4/84 PAR 1

--- src/video/presets/kaypro.ts
+++ src/video/presets/kaypro.ts
@@ -20,7 +20,7 @@ export const kaypro2Presets = [
 ];
 
 export const kaypro4Presets = [
-  T('kaypro4-text-80x25', 'kaypro-4', 'Kaypro 4/84', 80, 25, 8, 16, 640, 400, 1/2,
+  T('kaypro4-text-80x25', 'kaypro-4', 'Kaypro 4/84', 80, 25, 8, 16, 640, 400, 1,
     'kaypro4.u9', 'kaypro-4', 256, f8(16, 7),
     m(6, 5, 6, 5), '#33ff33', '#000000', '#000000', '#e0d8c8', '#b0a890', kayproPal),
 
