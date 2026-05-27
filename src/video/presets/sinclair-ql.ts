7c7440de5549ca2a2a2c8941be8f9e37a3585ecf
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2026-05-27 12:40:39 +0200

    fix: QL font cellHeight=8, C128 VIC-II mode, remove broken +3 mode

--- src/video/presets/sinclair-ql.ts
+++ src/video/presets/sinclair-ql.ts
@@ -5,10 +5,10 @@ const qlPal = ['#000000','#0000ff','#ff0000','#ffff00','#000000','#0000ff','#ff0
 
 export const sinclairQlPresets = [
   T('ql-mode4-64x24', 'sinclair-ql', 'Sinclair QL', 64, 24, 8, 8, 512, 192, 2/3,
-    'kaypro4.u9', 'kaypro-4', 256, f8(8, 7),
+    'kaypro4.u9', 'sinclair-ql', 256, f8(8, 7),
     m(8, 6, 8, 6), '#ffffff', '#000000', '#000000', '#1a1a1a', '#444', qlPal),
 
   T('ql-mode8-32x24', 'sinclair-ql', 'Sinclair QL', 32, 24, 8, 8, 256, 192, 1,
-    'kaypro4.u9', 'kaypro-4', 256, f8(8, 7),
+    'kaypro4.u9', 'sinclair-ql', 256, f8(8, 7),
     m(8, 6, 8, 6), '#ffffff', '#000000', '#000000', '#1a1a1a', '#444', qlPal),
 ];
