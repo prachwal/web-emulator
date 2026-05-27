02a4024c44c9d2f5d85289f80dcccc8a6b2f11cb
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2026-05-27 09:52:43 +0200

    fix(otrona): cell 8x10 with PAR 0.5 for 2x viewport scale

--- src/video/presets/otrona.ts
+++ src/video/presets/otrona.ts
@@ -4,7 +4,7 @@ const otrPal = ['#000000','#ffb347','#ffb347','#ffb347','#ffb347','#ffb347','#ff
   '#ffb347','#ffb347','#ffb347','#ffb347','#ffb347','#ffb347','#ffb347','#ffb347'];
 
 export const otronaPresets = [
-  T('otrona-text-80x24', 'otrona', 'Otrona Attache', 80, 24, 8, 16, 640, 384, 1,
-    'otrona-g-chargen.bin', 'otrona', 256, f8(16, 7),
+  T('otrona-text-80x24', 'otrona', 'Otrona Attache', 80, 24, 8, 10, 640, 240, 1/2,
+    'otrona-g-chargen.bin', 'otrona', 256, f8(10, 7),
     m(8, 6, 8, 6), '#ffb347', '#000000', '#000000', '#303030', '#666', otrPal),
 ];
