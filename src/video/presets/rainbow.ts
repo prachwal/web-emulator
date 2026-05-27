b5b33d84fc7d91f420deded1abdc6ef8b2e36b0d
Przemyslaw Rachwal <przemyslaw.rachwal@gmail.com>
2026-05-27 12:05:58 +0200

    fix(rainbow): use real chargen from DEC bitsavers firmware (E98_020E3)

--- src/video/presets/rainbow.ts
+++ src/video/presets/rainbow.ts
@@ -4,7 +4,7 @@ const rnbPal = ['#000000','#33ff33','#33ff33','#33ff33','#33ff33','#33ff33','#33
   '#33ff33','#33ff33','#33ff33','#33ff33','#33ff33','#33ff33','#33ff33','#33ff33'];
 
 export const rainbowPresets = [
-  T('rainbow-text-80x24', 'rainbow', 'DEC Rainbow 100', 80, 24, 8, 10, 640, 240, 1/2,
-    'kaypro4.u9', 'rainbow-100', 256, f8(10, 7),
+  T('rainbow-text-80x24', 'rainbow', 'DEC Rainbow 100', 80, 24, 8, 8, 640, 192, 5/12,
+    'rainbow-chargen-8x8.bin', 'rainbow-100', 256, f8(8, 7),
     m(8, 6, 8, 6), '#33ff33', '#000000', '#000000', '#303030', '#666', rnbPal),
 ];
